674284cee0dc2148d79ecd19f726ecbaee7b0236
DavidMiao1127 <miaos22@mails.tsinghua.edu.cn>
2026-05-02 14:23:17 +0800

    feat: add legal skill extraction

---
+++ extract.sh
@@ -0,0 +1,3 @@
+/raid/miaoshuo/.venv/bin/python /raid/miaoshuo/LegalSkill/SkillX/batch_legal_extraction.py \
+    --folder "/raid/miaoshuo/LegalSkill/wenshu" \
+    --output "/raid/miaoshuo/LegalSkill/Skill"
